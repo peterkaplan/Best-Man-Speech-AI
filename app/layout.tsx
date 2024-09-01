22a3583ce348877d5001143d54f2db541a759eac
Peter Kaplan <pkap@umich.edu>
2024-08-31 22:39:33 -0700

    fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from '@/components/Navbar'
 import { Toaster } from "@/components/form/Toaster"
-import RestOfHomepage from "@/components/RestOfHomepage";
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
@@ -21,7 +20,6 @@ export default function RootLayout({
     <body> 
       <Navbar />
       {children}
-      <RestOfHomepage />
       <Toaster />
       <Analytics/> 
       <SpeedInsights/>
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from '@/components/Hero'
+import RestOfHomepage from '@/components/RestOfHomepage';
 
 export default function Home() {
   return (
